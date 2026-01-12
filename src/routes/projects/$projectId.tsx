5d5776d418cbd39e997837b4e27c07e0e7c4e812
wanglt <wanglt@startimes.com.cn>
2026-01-12 09:20:22 +0800

    feat(fileExplorer): migrate to useSuspenseQuery and preload data in route loader

--- src/routes/projects/$projectId.tsx
+++ src/routes/projects/$projectId.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 import { z } from "zod";
 import { ProjectIdLayout } from "@/components/project-id-layout";
 import { ProjectIdView } from "@/components/project-id-view";
+import { Spinner } from "@/components/ui/spinner";
 import { orpcClient } from "@/lib/orpc";
 
 const searchSchema = z.object({
@@ -11,14 +12,34 @@ const searchSchema = z.object({
 export const Route = createFileRoute("/projects/$projectId")({
   component: RouteComponent,
   validateSearch: searchSchema,
-  async loader(ctx) {
-    const currentUser = await ctx.context.queryClient.ensureQueryData(
+  async loader({ context: { queryClient }, params }) {
+    const currentUser = await queryClient.ensureQueryData(
       orpcClient.profile.getCurrentUser.queryOptions(),
     );
-
     if (!currentUser || !currentUser.id) {
       throw redirect({ to: "/sign-in" });
     }
+    const projectId = Number(params.projectId);
+    await Promise.all([
+      queryClient.ensureQueryData(
+        orpcClient.project.getById.queryOptions({
+          input: projectId,
+        }),
+      ),
+      queryClient.ensureQueryData(
+        orpcClient.file.getFileTree.queryOptions({
+          input: { projectId },
+        }),
+      ),
+    ]);
+  },
+
+  pendingComponent: () => {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <Spinner />
+      </div>
+    );
   },
 });
 
